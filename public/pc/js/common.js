9714a9e10be9d22a8da864569cc1f0884ac591bb
peanut-cream <1194163269@qq.com>
2018-01-12 15:19:07 +0800

    基本结构完成，并且登录功能完成

--- public/pc/js/common.js
+++ public/pc/js/common.js
@@ -2,6 +2,30 @@
  * Created by 伍 on 2018/1/11.
  */
 ;(function(){
+  //  进度条
+  NProgress.configure({ showSpinner: false });
+  $(window).ajaxStart(function(){
+    NProgress.start();
+  });
+  $(window).ajaxStop(function(){
+    setTimeout(function(){
+      NProgress.done();
+    },1000);
+  });
+
+if(location.href.indexOf("login.html")==-1){
+  $.ajax({
+    type:"get",
+    url:"/employee/checkRootLogin",
+    success:function(info){
+      if(info.error){
+        location.href="login.html";
+      }
+    }
+  })
+}
+
+
 //  二级菜单动画
 $(".category").on("click",function(){
   $(".second-ul").slideToggle();
@@ -11,15 +35,18 @@ $(".category").on("click",function(){
     $(".le_main").toggleClass("now");
   });
   $(".icon-loginout").on("click",function(){
-    //$.ajax({
-    //  type:"get",
-    //  url:"/employee/employeeLogout",
-    //  success:function(info){
-    //    //console.log(info);
-    //    if(info.success){
-    //      location.href="login.html";
-    //    }
-    //  }
-    //})
+    $('#modal').modal("show");
   });
+  $(".btn-loginout").on("click",function(){
+    $.ajax({
+      type:"get",
+      url:"/employee/employeeLogout",
+      success:function(info){
+        if(info.success){
+          location.href="login.html";
+        }
+      }
+    })
+  })
+
 })();
